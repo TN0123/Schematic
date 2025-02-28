49dfc0395795c2864a22f6423c0534d29a2e62b5
TN0123 <112415698+TN0123@users.noreply.github.com>
2025-02-28 15:01:31 -0500

    fix(api): fixed delete type for events

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -3,14 +3,14 @@ import { getServerSession } from "next-auth";
 import prisma from "@/lib/prisma";
 import authOptions from "@/lib/auth";
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, { params }: { params: Promise<{ id: string }> }) {
   const session = await getServerSession(authOptions);
   if (!session?.user?.id) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   try {
-    const { id } = await params;
+    const id = (await params).id;
     const event = await prisma.event.delete({
       where: { id: id, userId: session.user.id },
     });
