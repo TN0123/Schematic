7be76d7e76312b111b5638fad508b542326e2526
TN0123 <112415698+TN0123@users.noreply.github.com>
2025-02-28 14:58:21 -0500

    fix(api): fix attempt for delete and patch endpoints for bulletin

--- src/app/api/bulletins/[id]/route.ts
+++ src/app/api/bulletins/[id]/route.ts
@@ -5,7 +5,7 @@ import { authOptions } from '@/lib/auth';
 
 export async function PATCH(
   request: Request,
-  context: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const session = await getServerSession(authOptions);
   
@@ -14,7 +14,7 @@ export async function PATCH(
   }
 
   const updates = await request.json();
-  const { id } = await context.params;
+  const id = (await params).id;
   const bulletin = await prisma.bulletin.update({
     where: { id: id },
     data: updates,
@@ -25,14 +25,14 @@ export async function PATCH(
 
 export async function DELETE(
   request: Request,
-  context: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const session = await getServerSession(authOptions);
   
   if (!session?.user?.email) {
     return new NextResponse('Unauthorized', { status: 401 });
   }
-  const { id } = await context.params;
+  const id = (await params).id;
   await prisma.bulletin.delete({
     where: { id: id },
   });
