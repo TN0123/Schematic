acd513085b1044aa7c8f38342aa99408ce40459e
TN0123 <112415698+TN0123@users.noreply.github.com>
2025-04-04 10:36:21 -0400

    fix(schedule): UI bug in goal card styling

--- src/app/schedule/_components/GoalCard.tsx
+++ src/app/schedule/_components/GoalCard.tsx
@@ -7,11 +7,30 @@ interface GoalCardProps {
   removing: boolean;
 }
 
-const durationColors: Record<GoalDuration, string> = {
-  DAILY: "green",
-  WEEKLY: "blue",
-  MONTHLY: "yellow",
-  YEARLY: "purple",
+const durationColors: Record<
+  GoalDuration,
+  { border: string; bg: string; text: string }
+> = {
+  DAILY: {
+    border: "border-green-500",
+    bg: "bg-green-100",
+    text: "text-green-600",
+  },
+  WEEKLY: {
+    border: "border-blue-500",
+    bg: "bg-blue-100",
+    text: "text-blue-600",
+  },
+  MONTHLY: {
+    border: "border-yellow-500",
+    bg: "bg-yellow-100",
+    text: "text-yellow-600",
+  },
+  YEARLY: {
+    border: "border-purple-500",
+    bg: "bg-purple-100",
+    text: "text-purple-600",
+  },
 };
 
 export default function GoalCard({
@@ -21,17 +40,15 @@ export default function GoalCard({
 }: GoalCardProps) {
   return (
     <div
-      className={`flex w-full border-l-4 rounded-md items-center justify-between transition-all duration-500 shadow-sm hover:shadow-md hover:-translate-y-1 border-${
-        durationColors[goal.type]
-      }-500 bg-${durationColors[goal.type]}-100 ${
-        removing ? "-translate-x-full opacity-0" : "translate-x-0 opacity-100"
-      }`}
+      className={`flex w-full rounded-md border-l-4 items-center justify-between transition-all duration-500 shadow-sm hover:shadow-md hover:-translate-y-1 
+    ${durationColors[goal.type].border} ${durationColors[goal.type].bg} 
+    ${removing ? "-translate-x-full opacity-0" : "translate-x-0 opacity-100"}`}
     >
       <div className="flex w-full px-4 py-2 text-center justify-center items-center">
         <span
-          className={`absolute left-2 text-${
-            durationColors[goal.type]
-          }-600 text-xs font-semibold`}
+          className={`absolute left-2 ${
+            durationColors[goal.type].text
+          } text-xs font-semibold`}
         >
           {goal.type.charAt(0).toUpperCase()}
         </span>
